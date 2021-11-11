ee969c9e09cba4d8d1d2555f697af821b907db10
Joao Meyer <1994meyer@gmail.com>
2021-11-11 01:45:56 -0300

    Made API section work

--- site/src/auth.api.js
+++ site/src/auth.api.js
@@ -1,9 +1,13 @@
 import { Auth0Client } from '@auth0/auth0-spa-js'
 
 const auth0 = new Auth0Client({
-  domain: 'dev-1ymtsnz6.us.auth0.com', // Get this on Auth0's site
-  client_id: '41TAaJZdJzt6Q0bnhBHgEsomhLhDdpSn', // Get this on Auth0's site
-  redirect_uri: 'http://localhost:8080/#/callback' // Configure on Auth0's site
+  // Defined on Applications section of Auth0
+  domain: 'dev-1ymtsnz6.us.auth0.com',
+  client_id: '41TAaJZdJzt6Q0bnhBHgEsomhLhDdpSn',
+  redirect_uri: 'http://localhost:8000/#/callback',
+
+  // Create an API on API section of Auth0
+  audience: 'https://localhost:8080'
 })
 
 window.auth0 = auth0
--- site/src/views/HomeView.vue
+++ site/src/views/HomeView.vue
@@ -2,16 +2,37 @@
   <div>
     <button @click="login"> Login </button>
     <button @click="logout"> Logout </button>
+
     <br>
     <router-link to="/profile"> Profile (requires auth) </router-link>
+
+    <br>
+    <button @click="ping"> Ping (no auth) </button>
+    <br>
+    <pre>{{ pong }}</pre>
+
+    <br>
+    <button @click="auth"> Auth (requires auth) </button>
+    <br>
+    <pre>{{ data }}</pre>
+
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
+import auth0 from '@/auth.api'
+
 import { LOGIN, LOGOUT } from '@/store/type.actions'
 
 export default {
   name: 'HomeView',
+
+  data: () => ({
+    pong: null,
+    data: null
+  }),
 
   methods: {
     login () {
@@ -19,6 +40,20 @@ export default {
     },
     logout () {
       this.$store.dispatch(LOGOUT)
+    },
+    async ping () {
+      console.log('Calling /ping')
+      this.pong = await axios.get('http://localhost:8080/ping')
+    },
+    async auth () {
+      console.log('Calling /auth')
+      const token = await auth0.getTokenSilently()
+      console.log(token)
+      this.data = await axios.get('http://localhost:8080/auth', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
     }
   }
 }
