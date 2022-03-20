44841ec402c529b704e9c8fe5750c91e780d50b1
Joao Meyer <1994meyer@gmail.com>
2022-03-20 12:00:53 +0000

    Fixingin lint

--- site/.eslintrc.js
+++ site/.eslintrc.js
@@ -17,10 +17,5 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ]
+  }
 }
